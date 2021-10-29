5d521b5bf75427a2ce01bc4f3f77e558a6c12491
JuAnXyber <89549253+JuAnXyber@users.noreply.github.com>
2021-10-29 09:08:08 +0700

    Create gay.py

---
+++ gay.py
@@ -0,0 +1,14 @@
+print("YOU GAY?")
+print("YES or NO")
+a=input("")
+
+if a=='no' or 'NO':
+ print("if YOU not GAYY, does your MOTHER know you GAY ???")
+ print("YES or NO")
+ b=input("")
+ if b=='YES' or 'yes': print("IT MEANS YOUR MOTHER KNOW IF YOU GAYY!"*1000000)
+ if b=='no' or 'NO': print("IT MEANS YOU ARE GAYY!"*1000000)
+ 
+if a=='yes' or 'YES':
+ print("YOU ARE GAYYY!"*1000000)
+   
